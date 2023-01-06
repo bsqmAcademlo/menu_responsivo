681784049d4ffacec3d61f23b48653b820e9f30b
bsqmAcademlo <brayan.munoz@academlo.com>
2023-01-06 12:12:30 -0500

    firts commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu li a");
+const bxMenu = document.querySelector(".bx-menu");
+
+function handleMenu() {
+    menu.classList.toggle("menu_show");
+}
+
+bxMenu.addEventListener("click", handleMenu);
+
+links.forEach((link) => {
+    link.addEventListener("click", handleMenu);
+});
